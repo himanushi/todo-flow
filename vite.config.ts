10a99a6648e780348ff13f1b58d8105e4966aab4
himanushi <himanushi@users.noreply.github.com>
2024-09-03 20:29:25 +0900

    ビルド設定の追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,31 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		minify: true,
+		lib: {
+			entry: "src/main.tsx",
+			name: "todo",
+			formats: ["es"],
+		},
+		rollupOptions: {
+			output: {
+				globals: {
+					react: "React",
+				},
+				preserveModules: true,
+				inlineDynamicImports: false,
+				entryFileNames: ({ name: fileName }) => `${fileName}.js`,
+			},
+			treeshake: {
+				preset: "smallest",
+				annotations: true,
+				moduleSideEffects: true,
+				correctVarValueBeforeDeclaration: true,
+				propertyReadSideEffects: true,
+				tryCatchDeoptimization: true,
+				unknownGlobalSideEffects: true,
+			},
+		},
+	},
 });
