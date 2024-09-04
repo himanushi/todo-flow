f3f34e658336fbb82acfd2a0db7a9412fe320e4b
himanushi <himanushi@users.noreply.github.com>
2024-09-04 19:39:25 +0900

    スタイルの変更による背景色とボーダーの追加

--- src/PromptNode.tsx
+++ src/PromptNode.tsx
@@ -1,3 +1,5 @@
 export const PromptNode = () => {
-	return <div className="flex col p-3 bg-purple-300">aaa</div>;
+	return (
+		<div className="flex col p-3 bg-white border-gray-700 border-2">aaa</div>
+	);
 };
