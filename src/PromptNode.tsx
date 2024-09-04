e056a8200bdc3e48bdc1a1a8856dd254e754ea79
himanushi <himanushi@users.noreply.github.com>
2024-09-04 21:07:53 +0900

    コンポーネントにテキストエリア自動サイズ変更機能を追加

--- src/PromptNode.tsx
+++ src/PromptNode.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+
 export const PromptNode = () => {
+	const [value, setValue] = useState("");
+
 	return (
-		<div className="flex col p-3 bg-white border-gray-700 border-2">aaa</div>
+		<div className="flex w-52 flex-col items-center justify-center gap-2 rounded-lg border-2 border-gray-900 bg-teal-300 p-3">
+			<TextareaAutosize
+				className="w-full resize-none rounded-lg border-2 border-gray-900 px-3 py-2"
+				maxRows={5}
+				minRows={2}
+				onChange={(e) => {
+					setValue(e.target.value);
+				}}
+				value={value}
+			/>
+			<button
+				type="button"
+				className={
+					"rounded-lg border-2 border-gray-900 bg-rose-500 px-3 transition-all duration-150 active:translate-y-1"
+				}
+			>
+				やる
+			</button>
+		</div>
 	);
 };
