c9c3446a6582282021d7248e83eba7d0b9ce6af0
himanushi <himanushi@users.noreply.github.com>
2024-09-06 20:54:42 +0900

    useEffectを追加してAPIキーを保存・取得する処理の実装

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,18 @@
 import { Panel } from "@xyflow/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Icon } from "./Icon";
 import { Popover } from "./Popover";
 
 export const Settings = () => {
 	const [open, setOpen] = useState(false);
 	const [apiKey, setApiKey] = useState("");
+
+	useEffect(() => {
+		const storedApiKey = localStorage.getItem("openai-api-key");
+		if (storedApiKey) {
+			setApiKey(storedApiKey);
+		}
+	}, []);
 
 	return (
 		<Panel position="top-right" onClick={() => setOpen(true)}>
@@ -16,10 +23,13 @@ export const Settings = () => {
 					<div className="flex flex-col gap-1 rounded-lg border-2 border-gray-800 bg-white p-3">
 						<p>OpenAI API Key</p>
 						<input
-							type="text"
+							type="password"
 							placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxx"
 							value={apiKey}
-							onChange={(e) => setApiKey(e.target.value)}
+							onChange={(e) => {
+								setApiKey(e.target.value);
+								localStorage.setItem("openai-api-key", e.target.value);
+							}}
 							className="w-full rounded-lg border-2 border-gray-800 px-3 py-2"
 						/>
 					</div>
