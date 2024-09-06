8e9a7bdec11eb03e540aa3636aad524ad80a617d
himanushi <himanushi@users.noreply.github.com>
2024-09-06 20:17:22 +0900

    設定コンポーネントの追加

---
+++ src/components/Settings.tsx
@@ -0,0 +1,36 @@
+import { Panel } from "@xyflow/react";
+import { useState } from "react";
+import { Icon } from "./Icon";
+import { Popover } from "./Popover";
+
+export const Settings = () => {
+	const [open, setOpen] = useState(false);
+	const [apiKey, setApiKey] = useState("");
+
+	return (
+		<Panel position="top-right" onClick={() => setOpen(true)}>
+			<Popover
+				open={open}
+				setOpen={setOpen}
+				popupContent={
+					<div className="flex flex-col gap-1 rounded-lg border-2 border-gray-800 bg-white p-3">
+						<p>OpenAI API Key</p>
+						<input
+							type="text"
+							placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxx"
+							value={apiKey}
+							onChange={(e) => setApiKey(e.target.value)}
+							className="w-full rounded-lg border-2 border-gray-800 px-3 py-2"
+						/>
+					</div>
+				}
+				targetContent={
+					<Icon
+						name="settings"
+						className="cursor-pointer select-none text-4xl text-gray-600 focus:outline-none"
+					/>
+				}
+			/>
+		</Panel>
+	);
+};
