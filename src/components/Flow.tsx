2c9b0e628a4f2b359dc2b1877a7ed2d69eb06ca5
himanushi <himanushi@users.noreply.github.com>
2024-09-07 20:16:47 +0900

    初期ノードの設定とスクリーンサイズに基づくエッジの更新削除

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,77 +1,34 @@
 import {
 	Background,
 	BackgroundVariant,
-	type Connection,
 	Controls,
 	type Edge,
 	MiniMap,
 	type Node,
 	ReactFlow,
-	addEdge,
 	useEdgesState,
 	useNodesState,
 } from "@xyflow/react";
-import {
-	type Dispatch,
-	type SetStateAction,
-	useCallback,
-	useEffect,
-	useState,
-} from "react";
 import "@xyflow/react/dist/style.css";
 import { PromptNode } from "./PromptNode";
 import { Settings } from "./Settings";
-import { YesNoNode } from "./YesNoNode";
 
+const initialNodes: Node[] = [
+	{
+		id: "prompt",
+		position: {
+			x: window.innerWidth / 2 - 104,
+			y: window.innerHeight / 2 - 100,
+		},
+		data: {},
+		type: "prompt",
+	},
+];
 const initialEdges: Edge[] = [];
 
-const nodeTypes = (setNodes: Dispatch<SetStateAction<Node[]>>) => ({
-	prompt: PromptNode({ setNodes }),
-    yesno: YesNoNode,
-});
-
 export const Flow = () => {
-	const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
+	const [nodes, setNodes, onNodesChange] = useNodesState<Node>(initialNodes);
 	const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>(initialEdges);
-	const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
-
-	useEffect(() => {
-		setScreenSize({
-			width: window.innerWidth,
-			height: window.innerHeight,
-		});
-	}, []);
-
-	useEffect(() => {
-		const initialNodes: Node[] = [
-			{
-				id: "prompt",
-				position: {
-					x: screenSize.width / 2 - 104,
-					y: screenSize.height / 2 - 100,
-				},
-				data: {},
-				type: "prompt",
-			},
-		];
-		setNodes(initialNodes);
-	}, [screenSize, setNodes]);
-
-	useEffect(() => {
-		if (nodes.length > 1) {
-			const newEdges = nodes.slice(1).map((node) => ({
-				id: `edge-${node.id}`,
-				source: "prompt",
-				target: node.id,
-			}));
-			setEdges((eds) => [...eds, ...newEdges]);
-		}
-	}, [nodes, setEdges]);
-
-	const onConnect = useCallback(
-		(params: Connection) => setEdges((eds) => addEdge(params, eds)),
-		[setEdges],
-	);
 
 	return (
 		<div style={{ width: "100vw", height: "100vh" }}>
@@ -80,8 +37,9 @@ export const Flow = () => {
 				edges={edges}
 				onNodesChange={onNodesChange}
 				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				nodeTypes={nodeTypes(setNodes)}
+				nodeTypes={{
+					prompt: PromptNode({ setNodes, setEdges }),
+				}}
 			>
 				<Settings />
 				<Controls />
@@ -91,4 +49,3 @@ export const Flow = () => {
 		</div>
 	);
 };
-import { type Edge } from "@xyflow/react";
